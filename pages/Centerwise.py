f602be8f2806bd7125365b39a0438b6e92c7badb
AZAMIQBAL <31731582+AZAMIQBAL@users.noreply.github.com>
2023-02-08 14:43:25 +0500

    Update Centerwise.py

--- pages/Centerwise.py
+++ pages/Centerwise.py
@@ -14,7 +14,22 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 
 
+#   Page configuration
+st.set_page_config(
+    page_title="DASHBOARD Polio",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
+#AZAM CODE Feature 1 Minimalize the Defaut (ap hide kar sakty ho header and footer jis pa streamlit likha hota ha)
+hide_menu_style = """
+    <style>
+    MainMenue {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 # loading data and showing it as a table on top of the dashboard
 #https://docs.google.com/spreadsheets/d/13ikD5WpjmapBlKY4j2dTrwsZ-du3pdfpHhjIYwugcxM/edit?usp=sharing
 
